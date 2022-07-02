ff9c6fc995bef3634c30543b124159698e845607
JooGraced <awuah1132@gmail.com>
2022-07-02 05:17:04 +0000

    This is my first Commit to this PROJECT

---
+++ links/views.py
@@ -0,0 +1,24 @@
+from .models import Link
+from .serializers import LinkSerializer
+from rest_framework import generics
+
+
+class PostListApi(generics.ListAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostCreateApi(generics.CreateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostDetail(generics.RetrieveAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostUpdateApi(generics.UpdateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostDeleteApi(generics.DestroyAPIView):
+    queryset= Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
